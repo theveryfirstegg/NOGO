3b395e29122cbb1cb931666cdae5df61203e1298
theveryfirstegg <114595197+theveryfirstegg@users.noreply.github.com>
2023-08-30 13:39:13 -0400

    Add files via upload

--- LogInScreen.js
+++ LogInScreen.js
@@ -1,17 +1,16 @@
 import {
   Text,
   View,
-  StyleSheet,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-
-import users from "../userInfo.json";
 import { useState } from "react";
+import styles from "./LogInScreen.styles";
+import users from "../data/proxy/users.json";
 
-const LogInScreen = () => {
-  const nav = useNavigation();
+const LogInScreen = ({ navigation }) => {
   const [userName, setUserName] = useState("");
   const [userPassword, setPassword] = useState("");
   const [hidePassword, setHidePassword] = useState(true);
@@ -27,121 +26,50 @@ const LogInScreen = () => {
     );
 
     if (found) {
-      nav.navigate("Main");
+      navigation.navigate("Main");
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.loginTitle}>Log In</Text>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.loginTitle}>Log In</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={setUserName}
-        autoCapitalize="none"
-      />
-      <View style={styles.passwordContainer}>
         <TextInput
-          style={styles.passwordInput}
-          placeholder="Password"
-          onChangeText={setPassword}
-          secureTextEntry={hidePassword}
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setUserName}
           autoCapitalize="none"
         />
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            placeholder="Password"
+            onChangeText={setPassword}
+            secureTextEntry={hidePassword}
+            autoCapitalize="none"
+          />
 
-        <TouchableOpacity
-          style={styles.showButton}
-          onPress={handleShowPassword}
-        >
-          <Text style={{ fontWeight: 600 }}>
-            {hidePassword ? "Show" : "Hide"}
-          </Text>
+          <TouchableOpacity
+            style={styles.showButton}
+            onPress={handleShowPassword}
+          >
+            <Text style={{ fontWeight: 600 }}>
+              {hidePassword ? "Show" : "Hide"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogIn}>
+          <Text style={styles.logIn}>Log In</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.forgotPassword}>Forgot your password?</Text>
         </TouchableOpacity>
       </View>
-
-      <TouchableOpacity style={styles.loginButton} onPress={handleLogIn}>
-        <Text style={styles.logIn}>Log In</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
-        <Text style={styles.forgotPassword}>Forgot your password?</Text>
-      </TouchableOpacity>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default LogInScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#ffd401",
-  },
-
-  loginTitle: {
-    fontSize: 35,
-    fontWeight: "600",
-    textAlignVertical: "top",
-    marginTop: 60,
-    marginBottom: 20,
-  },
-
-  loginButton: {
-    width: 340,
-    height: 52,
-    justifyContent: "center",
-    backgroundColor: "black",
-    borderRadius: 30,
-    marginBottom: 15,
-  },
-
-  logIn: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 18,
-    textAlign: "center",
-  },
-
-  input: {
-    width: 350,
-    height: 50,
-    padding: 12,
-    borderRadius: 7,
-    backgroundColor: "#ffffff",
-    color: "black",
-    marginBottom: 15,
-  },
-
-  passwordInput: {
-    width: 350,
-    height: 50,
-    padding: 12,
-    borderRadius: 7,
-    backgroundColor: "#ffffff",
-    color: "black",
-    marginBottom: 15,
-    paddingRight: 58,
-  },
-
-  passwordContainer: {
-    flexDirection: "row",
-  },
-
-  showButton: {
-    position: "absolute",
-    right: 15,
-    top: 14,
-    width: 35,
-    height: 20,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  forgotPassword: {
-    fontWeight: "600",
-    fontSize: 18,
-  },
-});
--- MainScreen.js
+++ MainScreen.js
@@ -1,433 +1,276 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-mixed-spaces-and-tabs */
 import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Pressable,
-  Dimensions
-} from "react-native";
-import { useState, useEffect, useMemo, useRef, useCallback } from "react";
-import WheelPicker from "react-native-wheely";
-import DateTimePicker from "@react-native-community/datetimepicker";
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	Keyboard
+} from 'react-native'
+import { useState, useMemo, useRef, useCallback } from 'react'
+import dayjs from 'dayjs'
+import WheelPicker from 'react-native-wheely'
+import DateTimePicker from '@react-native-community/datetimepicker'
 import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-  BottomSheetBackdrop,
-} from "@gorhom/bottom-sheet";
-import { useNavigation } from "@react-navigation/native";
-import states from "../states.json";
-import cars from "../carInfo.json";
+	BottomSheetModal,
+	BottomSheetModalProvider,
+	BottomSheetBackdrop,
+} from '@gorhom/bottom-sheet'
+import styles from './MainScreen.styles'
+import states from '../data/states.json'
+import cars from '../data/cars.json'
+import locations from '../data/locations.json'
 
-const MainScreen = () => {
-  const [selectedIndex, setSelectedIndex] =
-    useState(''); /* State for location */
-  const [date, setDate] = useState(
-    new Date(1598051730000)
-  ); /* State for date */
-  const [selectedStateIndex, setSelectedStateIndex] =
-    useState(0); /* State for state */
-  const [plateNumber, onChangePlateNum] =
-    useState(""); /* State for plate number */
+const MainScreen = ({ navigation }) => {
+	const bottomSheetModalRef = useRef(null)
+	const [type, setType] = useState('location')
+	const [option, setOption] = useState({
+		location: { current: 0, value: 0 },
+		date: { current: new Date(), value: new Date() },
+		make: { current: 0, value: 0 },
+		model: { current: 0, value: 0 },
+		state: { current: 0, value: states.findIndex((elem) => elem === 'Tennessee') },
+		plate: { current: '', value: '' },
+		color: { current: '', value: '' }
+	})
 
-  const [carData, setCarData] = useState([]);
-  const [selectedCarIndex, setCarIndex] = useState(0);
+	const datetime = useMemo(() => dayjs(option.date.value).format('MMMM D, YYYY @h:mm A'), [option.date.value])
 
-  const [isSuccess, setSuccess] = useState(false);
-  const [carTypeIndex, setCarType] = useState(0);
-  const [carTypeData, setCarTypeData] = useState([]);
+	const carMakes = useMemo(() => cars.map((elem) => elem.brand), [])
 
-  const [carModelChosen, setCarModelChosen] = useState(false);
+	const carModelsCurrent = useMemo(() => cars[option.make.current].models, [option.make.current])
 
-  const [carColor, setCarColor] = useState("");
+	const carModelsValue = useMemo(() => cars[option.make.value].models, [option.make.value])
 
-  const nav = useNavigation();
+	const isReadyToSubmit = useMemo(() => {
+		for (const [key] of Object.entries(option)) {
+			if (option[key].value === '') {
+				return false
+			}
+		}
+		return true
+	}, [option])
 
-  /* Stat */
-  const [locationSelected, setLocation] = useState(false);
-  const [dateTimeSelected, setDateTime] = useState(false);
-  const [modelSelected, setModel] = useState(false);
-  const [stateSelected, setState] = useState(false);
+	const updateOption = (value, where='current', theType=type) => {
+		setOption((prevOption) => ({ 
+			...prevOption,
+			[theType]: { 
+            	...prevOption[theType],
+            	[where]: value
+			} 
+		}))
+	}
 
-  const windowWidth = Dimensions.get('window').width;
+	const handleOptionSelect = () => {
+		bottomSheetModalRef.current.close()
+		updateOption(option[type].current, 'value')
+	}
 
-  useEffect(() => {
-    result = [];
-    cars.map((elem) => result.push(elem));
-    setCarData(result);
-    setSuccess(true);
-  }, []);
+	const RenderOptionsLocation = useCallback(() => (
+		<WheelPicker
+			selectedIndex={option.location.current}
+			options={locations}
+			onChange={(index) => updateOption(index)}
+			itemTextStyle={styles.optionsStyle} 
+		/>
+	), [option.location])
 
-  const getBrand = (jsonObject) => {
-    result = [];
-    cars.map((elem) => result.push(elem.brand));
-    return result;
-  };
+	const RenderOptionsDate = useCallback(() => (
+		<DateTimePicker
+			value={option.date.current}
+			mode="datetime"
+			is24Hour
+			onChange={(e, selectedDate) => updateOption(selectedDate)}
+			display="spinner"
+			style={{ marginBottom: -16 }}
+		/>
+	), [option.date])
 
-  const handleCarChange = (index) => {
-    setCarIndex(index);
-    setCarModelChosen(true);
-  };
+	const RenderCarMake = useCallback(() => (
+		<WheelPicker
+			selectedIndex={option.make.current}
+			options={carMakes}
+			onChange={(index) => {
+				updateOption(index)
+				updateOption(0, 'value', 'model') // reset car model
+			}}
+			itemTextStyle={styles.optionsStyle}
+		/>
+	), [option.make])
 
-  /* Options for location wheel picker */
-  const locations = ['Location 1', 'Location 2', 'Location 3', 'Location 4']
+	const RenderCarModel = useCallback(() => (
+		<WheelPicker
+			selectedIndex={option.model.current}
+			options={carModelsCurrent}
+			onChange={(index) => updateOption(index)}
+			itemTextStyle={styles.optionsStyle}
+		/>
+	), [option.model])
 
-  /* Bottom Sheets */
+	const RenderState = useCallback(() => (
+		<WheelPicker
+			selectedIndex={option.state.current}
+			options={states}
+			onChange={(index) => updateOption(index)}
+			itemTextStyle={styles.optionsStyle}
+		/>
+	), [option.state])
 
-  /* Reference and methods for location bottom sheet */
-  const bottomSheetModalRef = useRef(null);
+	const RenderPickerView = useCallback(({ type: theType }) => {
+		switch (theType) {
+            
+		case 'location':
+			return (
+				<RenderOptionsLocation />
+			)
+		case 'date':
+			return (
+				<RenderOptionsDate />
+			)
+		case 'make':
+			return (
+				<RenderCarMake />
+			)
+		case 'model':
+			return (
+				<RenderCarModel />
+			)
+		case 'state':
+			return (
+				<RenderState />
+			)
+		default:
+			return null
+		}
+	}, [RenderOptionsLocation, RenderOptionsDate, RenderCarMake, RenderCarModel, RenderState])
 
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
-  }, []);
+	const handlePresentModal = (theType) => {
+		setType(theType)
+		updateOption(option[theType].value, 'current', theType)
+		bottomSheetModalRef.current?.present()
+	}
 
-  const handleClosePress = () => {
-    bottomSheetModalRef.current.close();
-    setLocation(true);
-  };
+	/* Bottom Sheet Backdrops */
+	const renderBackdrop = useCallback(
+		(props) => (
+			<BottomSheetBackdrop
+				{...props}
+				opacity={0.3}
+				disappearsOnIndex={-1}
+				appearsOnIndex={0}
+				pressBehavior="close"
+			/>
+		),
+		[]
+	)
 
-  /* Reference and methods for date/time bottom sheet */
-  const dateBottomSheet = useRef(null);
+	const DismissKeyboard = ({children}) => (
+		<TouchableWithoutFeedback
+		onPress={() => Keyboard.dismiss()}>
+			 {children}
+		</TouchableWithoutFeedback>
 
-  const handleDateTimePress = useCallback(() => {
-    dateBottomSheet.current?.present();
-  }, []);
+	);
 
-  const handleDateTimeClose = () => {
-    dateBottomSheet.current.close();
-    setDateTime(true);
-  };
+	return (
+		<BottomSheetModalProvider>
+			<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<View style={styles.container}>
+				<Text style={styles.mainScreenTitle}>Vehicle Details</Text>
+				<TextInput onBlur={Keyboard.dismiss()}
+					value={locations[option.location.value]}
+					style={styles.input}
+					placeholder="Location"
+					readOnly
+					onPressIn={() => handlePresentModal('location')}
+				/>
+		
+				<TextInput
+					value={datetime}
+					style={styles.input}
+					placeholder="Date/time"
+					readOnly
+					onPressIn={() => handlePresentModal('date')}
+				/>
+	
+				<TextInput
+					value={carMakes[option.make.value]}
+					style={styles.input}
+					placeholder="Make"
+					readOnly
+					onPressIn={() => handlePresentModal('make')}
+				/>
+				<TextInput
+					value={carModelsValue[option.model.value]}
+					style={styles.input}
+					placeholder="Model"
+					readOnly
+					onPressIn={() => handlePresentModal('model')}
+					
+				/>
+				
+				<TextInput
+					style={styles.input}
+					placeholder="Color"
+					value={option.color.value}
+					onChangeText={(value) => updateOption(value, 'value', 'color')}
+				/>
+				
 
-  /* Reference and methods for car make sheet */
-  const makeBottomSheet = useRef(null);
+				<TextInput
+					value={states[option.state.value]}
+					style={styles.input}
+					placeholder="State"
+					readOnly
+					onPressIn={() => handlePresentModal('state')}
+				/>
+				<TextInput
+					style={styles.input}
+					value={option.plate.value}
+					onChangeText={(value) => updateOption(value, 'value', 'plate')}
+					placeholder="License Plate"
+				/>
 
-  const handleMakePress = useCallback(() => {
-    makeBottomSheet.current?.present();
-  }, []);
+				<TouchableOpacity
+					style={[styles.submitButton,
+						!isReadyToSubmit && styles.submitButtonDisabled
+					]}
+					disabled={!isReadyToSubmit}
+					onPress={() => { navigation.navigate('Success')}}
+				>
+					<Text style={[
+						styles.submitText,
+						!isReadyToSubmit && styles.submitTextDisabled
+					]}>Submit</Text>
+				</TouchableOpacity>
 
-  const handleMakeClose = () => {
-    makeBottomSheet.current.close();
-    setCarModelChosen(true);
-  };
+				<BottomSheetModal
+					ref={bottomSheetModalRef}
+					index={0}
+					snapPoints={useMemo(() => ['40%'], [])}
+					stackBehavior="replace"
+					backdropComponent={renderBackdrop}
+					handleStyle={styles.sheetHandle}
+				>
+					<View style={styles.innerContainerSheet}>
+						<View style={styles.innerContainerSheetHeader}>
+							<TouchableOpacity
+								activeOpacity={0.8}
+								style={styles.innerContainerButton}
+								onPress={handleOptionSelect}>
+								<Text style={styles.innerContainerSheetHeaderText}>
+                                    Done
+								</Text>
+							</TouchableOpacity>
+						</View>
+						<RenderPickerView type={type} />
+					</View>
+				</BottomSheetModal>
+			</View>
+			</TouchableWithoutFeedback>
+		</BottomSheetModalProvider>
+	)
+}
 
-  /* Reference and methods for model sheet */
-  const modelBottomSheet = useRef(null);
-
-  const handleModelPress = useCallback(() => {
-    modelBottomSheet.current?.present();
-  }, []);
-
-  const handleModelClose = () => {
-    modelBottomSheet.current.close();
-    setModel(true);
-  };
-
-  /* Reference and methods for state sheet */
-  const stateBottomSheet = useRef(null);
-
-  const handleStatePress = useCallback(() => {
-    stateBottomSheet.current?.present();
-  }, []);
-
-  const handleStateClose = () => {
-    stateBottomSheet.current.close();
-    setState(true);
-  };
-
-  /* Bottom Sheet Backdrops */
-  const renderBackdrop = useCallback(
-    (props) => (
-      <BottomSheetBackdrop
-        {...props}
-        opacity={0.3}
-        disappearsOnIndex={-1}
-        appearsOnIndex={0}
-        pressBehavior={"close"}
-      />
-    ),
-    []
-  );
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.mainScreenTitle}>Vehicle Details</Text>
-
-      <BottomSheetModalProvider>
-        <TextInput
-          style={styles.input}
-          placeholder="Location"
-          readOnly={true}
-          onPressIn={handlePresentModalPress}
-        />
-
-        <BottomSheetModal
-          ref={bottomSheetModalRef}
-          index={0}
-          snapPoints={useMemo(() => ["40%"], [])}
-          stackBehavior="replace"
-          backdropComponent={renderBackdrop}
-        >
-          <View style={styles.locationSheet}>
-            <WheelPicker
-              selectedIndex={selectedIndex}
-              options={["Location 1", "Location 2", "Location 3", "Location 4"]}
-              onChange={(index) => setSelectedIndex(index)}
-              itemTextStyle={styles.optionsStyle}
-            />
-
-            <TouchableOpacity
-              style={styles.confirmButton}
-              onPress={handleClosePress}
-            >
-              <Text style={styles.confirm}>Confirm</Text>
-            </TouchableOpacity>
-          </View>
-        </BottomSheetModal>
-
-        <TouchableOpacity>
-          <TextInput
-            style={styles.input}
-            placeholder="Date/time"
-            readOnly={true}
-            onPressIn={handleDateTimePress}
-          />
-        </TouchableOpacity>
-
-        <BottomSheetModal
-          ref={dateBottomSheet}
-          index={0}
-          snapPoints={useMemo(() => ["40%"], [])}
-          stackBehavior="replace"
-          backdropComponent={renderBackdrop}
-        >
-          <View style={styles.locationSheet}>
-            <DateTimePicker
-              value={date}
-              mode="datetime"
-              is24Hour={true}
-              onChange={(event, selectedDate) => {
-                if (event?.type === "dismissed") {
-                  setDate(date);
-                  return;
-                }
-                setDate(selectedDate);
-              }}
-              display="spinner"
-              style={{ marginBottom: -16 }}
-            />
-
-            <TouchableOpacity
-              style={styles.confirmButton}
-              onPress={handleDateTimeClose}
-            >
-              <Text style={styles.confirm}>Confirm</Text>
-            </TouchableOpacity>
-          </View>
-        </BottomSheetModal>
-
-        <TouchableOpacity>
-          <TextInput
-            style={styles.input}
-            placeholder="Make"
-            readOnly={true}
-            onPressIn={handleMakePress}
-          />
-        </TouchableOpacity>
-
-        <BottomSheetModal
-          ref={makeBottomSheet}
-          index={0}
-          snapPoints={useMemo(() => ["40%"], [])}
-          stackBehavior="replace"
-          backdropComponent={renderBackdrop}
-        >
-          <View style={styles.locationSheet}>
-            {isSuccess && (
-              <WheelPicker
-                selectedIndex={selectedCarIndex}
-                options={getBrand(cars)}
-                onChange={handleCarChange}
-                itemTextStyle={styles.optionsStyle}
-              />
-            )}
-
-            <Pressable style={styles.confirmButton} onPress={handleMakeClose}>
-              <Text style={styles.confirm}>Confirm</Text>
-            </Pressable>
-          </View>
-        </BottomSheetModal>
-
-        <TextInput
-          style={styles.input}
-          placeholder="Model"
-          readOnly={true}
-          onPressIn={() => {
-            setCarTypeData(cars[selectedCarIndex].models);
-            if (carModelChosen) {
-              handleModelPress();
-            }
-          }}
-        />
-
-        <BottomSheetModal
-          ref={modelBottomSheet}
-          index={0}
-          snapPoints={useMemo(() => ["40%"], [])}
-          stackBehavior="replace"
-          backdropComponent={renderBackdrop}
-        >
-          <View style={styles.locationSheet}>
-            {carModelChosen && (
-              <WheelPicker
-                selectedIndex={carTypeIndex}
-                options={carTypeData}
-                onChange={(index) => setCarType(index)}
-                itemTextStyle={styles.optionsStyle}
-              />
-            )}
-
-            <TouchableOpacity
-              style={styles.confirmButton}
-              onPress={handleModelClose}
-            >
-              <Text style={styles.confirm}>Confirm</Text>
-            </TouchableOpacity>
-          </View>
-        </BottomSheetModal>
-
-        <TextInput
-          style={styles.input}
-          placeholder="Color"
-          value={carColor}
-          onChangeText={setCarColor}
-        />
-
-        <TextInput
-          style={styles.input}
-          placeholder="State"
-          readOnly={true}
-          onPressIn={handleStatePress}
-        />
-
-        <BottomSheetModal
-          ref={stateBottomSheet}
-          index={0}
-          snapPoints={useMemo(() => ["40%"], [])}
-          stackBehavior="behavior"
-          backdropComponent={renderBackdrop}
-        >
-          <View style={styles.locationSheet}>
-            <WheelPicker
-              selectedIndex={selectedStateIndex}
-              options={JSON.parse(JSON.stringify(states)).states}
-              onChange={(index) => setSelectedStateIndex(index)}
-              itemTextStyle={styles.optionsStyle}
-            />
-
-            <TouchableOpacity
-              style={styles.confirmButton}
-              onPress={handleStateClose}
-            >
-              <Text style={styles.confirm}>Confirm</Text>
-            </TouchableOpacity>
-          </View>
-        </BottomSheetModal>
-
-        <TextInput
-          style={styles.input}
-          value={plateNumber}
-          onChangeText={onChangePlateNum}
-          placeholder="License Plate"
-        />
-
-        <TouchableOpacity
-          style={styles.submitButton}
-          onPress={() => {
-            if (
-              locationSelected &&
-              stateSelected &&
-              dateTimeSelected &&
-              carModelChosen &&
-              modelSelected &&
-              carColor !== "" &&
-              plateNumber !== ""
-            ) {
-              nav.navigate("Success");
-            }
-          }}
-        >
-          <Text style={styles.submitText}> Submit </Text>
-        </TouchableOpacity>
-      </BottomSheetModalProvider>
-    </View>
-  );
-};
-
-export default MainScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#ffd401",
-  },
-
-  locationSheet: {
-    flex: 1,
-  },
-
-  input: {
-    width: 350,
-    height: 50,
-    padding: 12,
-    borderRadius: 7,
-    backgroundColor: "#ffffff",
-    color: "black",
-    marginBottom: 15,
-  },
-
-  mainScreenTitle: {
-    fontSize: 35,
-    fontWeight: "600",
-    textAlignVertical: "top",
-    marginTop: 60,
-    marginBottom: 20,
-  },
-
-  confirmButton: {
-    width: Dimensions.get('window').width,
-    height: 90,
-    backgroundColor: "black",
-    marginTop: 50,
-    justifyContent: "center",
-    alignItems: 'center'
-  },
-
-  confirm: {
-    color: "white",
-    textAlign: "center",
-    fontWeight: "700",
-    fontSize: 18,
-  },
-
-  submitText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 18,
-    textAlign: "center",
-  },
-
-  submitButton: {
-    width: 340,
-    height: 52,
-    justifyContent: "center",
-    backgroundColor: "black",
-    borderRadius: 30,
-    marginTop: 170,
-  },
-
-  optionsStyle: {
-    fontSize: 20,
-  },
-});
+export default MainScreen
